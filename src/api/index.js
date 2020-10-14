9a268af5c9f408f78efa35d7927b70b4e6b54dd9
Alex <ialexjoshua@gmail.com>
2020-10-14 17:35:38 +0200

    feat: Component makes use of API call

--- src/api/index.js
+++
@@ -1,3 +0,0 @@
-export const API_URLS = {
-	getMarkets: 'https://api.bittrex.com/api/v1.1/public/getmarkets',
-};
--- src/components/market-row/component.jsx
+++ src/components/market-row/component.jsx
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
-import { API_URLS } from '../../api';
+import { API_URLS } from '../../api/constants';
 import { getAPIData } from '../../services/api';
 
 class MarketRow extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			hasLoaded: false,
+			coins: [],
+		};
+	}
+	async componentDidMount() {
+		const response = getAPIData(API_URLS.coinList);
+	}
+
 	render() {
-		const response = getAPIData(API_URLS.getMarkets);
-		console.log(response);
+		const { hasLoaded, coins } = this.state;
 		return <td>items</td>;
 	}
 }
