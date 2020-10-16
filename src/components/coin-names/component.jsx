60e057b1bfe9a94f416a92263a071afd3381a13c
Alex <ialexjoshua@gmail.com>
2020-10-16 11:23:47 +0200

    fix: Changed class to functional component

--- src/components/coin-names/component.jsx
+++ src/components/coin-names/component.jsx
@@ -1,32 +1,28 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import API_URLS from '../../api/constants';
 import storageKeys from '../../services/session-storage/constants';
 import getAPIData from '../../services/api';
-import { createCoinNameArray, getFirst20Items } from '../../utils';
+import { createCoinNameArray } from '../../utils';
 import { getFromSessionStorage } from '../../services/session-storage';
 
-class CoinNames extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			coinNames: [],
-		};
-	}
-	async componentDidMount() {
-		getAPIData(API_URLS.coinList, storageKeys.coinNames);
-		const storedCoinNames = getFromSessionStorage(storageKeys.coinNames);
-		const namesOfCoins = createCoinNameArray(storedCoinNames);
-		this.setState({
-			coinNames: namesOfCoins,
-		});
-	}
+const CoinNames = () => {
+  const [coinNames, setCoinNames] = useState([]);
 
-	render() {
-		const { coinNames } = this.state;
-		const coinsList = getFirst20Items(coinNames).map(coins => <li>{coins}</li>);
-		return <td>{coinsList}</td>;
-	}
-}
+  useEffect(() => {
+    getAPIData(API_URLS.coinList, storageKeys.coinNames);
+    const storedCoinNames = getFromSessionStorage(storageKeys.coinNames);
+    const namesOfCoins = createCoinNameArray(storedCoinNames);
+    setCoinNames(namesOfCoins);
+  }, []);
+
+  const coinNameList = coinNames.map((coins) => (
+    <tr key={coins}>
+      <td>{coins}</td>
+    </tr>
+  ));
+
+  return <>{coinNameList}</>;
+};
 
 export default CoinNames;
