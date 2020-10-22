b1e4ad596b683c7cd41871054c262ab75776e101
Alex <ialexjoshua@gmail.com>
2020-10-22 13:03:18 +0200

    fix: Removed references to session storage

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
 import { getFirst20Items } from '../../utils';
-import { saveToSessionStorage } from '../session-storage';
 
-const getAPIData = async (requestURL, storedKeys) => {
+const getAPIData = async (requestURL) => {
   try {
     const response = await axios
       .get(requestURL)
@@ -12,7 +11,6 @@ const getAPIData = async (requestURL, storedKeys) => {
       .catch((error) => error);
 
     if (response) {
-      saveToSessionStorage(storedKeys, response);
       return Promise.resolve(response);
     }
     return response;
