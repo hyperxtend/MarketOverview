77a6274fcd90e26247595883ef86a104877de9a7
Alex <ialexjoshua@gmail.com>
2020-10-23 12:59:17 +0200

    fix: Refactored API get request function

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -3,20 +3,12 @@ import axios from 'axios';
 import { getFirst20Items } from '../../utils';
 
 const getAPIData = async (requestURL) => {
-  try {
-    const response = await axios
-      .get(requestURL)
-      .then((request) => request)
-      .then((result) => getFirst20Items(result.data))
-      .catch((error) => error);
+  const response = await axios
+    .get(requestURL)
+    .then((result) => getFirst20Items(result.data))
+    .catch((error) => error);
 
-    if (response) {
-      return Promise.resolve(response);
-    }
-    return response;
-  } catch (error) {
-    return Promise.reject(error);
-  }
+  return response;
 };
 
 export default getAPIData;
