7ae8c8cc989bc2770aa4e5d3f8d86a9f1b944474
Alex <ialexjoshua@gmail.com>
2020-10-23 13:29:46 +0200

    feat: Added new API URL

--- src/api/constants/index.js
+++ src/api/constants/index.js
@@ -1,5 +1,6 @@
 const API_URLS = {
-	coinList: 'https://api.coingecko.com/api/v3/coins/list',
+  coinList: 'https://api.coingecko.com/api/v3/coins/list',
+  coinData: 'https://api.coingecko.com/api/v3/coins/',
 };
 
 export default API_URLS;
